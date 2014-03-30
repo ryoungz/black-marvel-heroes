f65e3ee5ebb2e3b9625f5c961d76141b10ec227c
Robert Young <ryoungz@gmail.com>
2014-03-30 12:07:29 -0500

    Initial commit

---
+++ marvel.js
@@ -0,0 +1,14 @@
+
+function Marvel($scope, $http) {
+
+    $scope.items = [];
+    $scope.getItems = function () {
+        $http({method: 'GET', url: 'http://gateway.marvel.com:80/v1/public/characters/1009629/comics?apikey=d6060228ef76a4849472fabfe5722656'})
+          .success(function (data, status) {
+              $scope.items = data;
+          })
+          .error(function (data, status) {
+              alert("Error");
+          });
+    };
+}
